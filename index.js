9c1bf41e2bfc994965df90b8fbeb31431cac0550
Diyor <diyorxasanov75@gmail.com>
2021-12-18 18:55:13 +0500

    create project

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.addEventListener("click",()=>{
+//     document.getElementById("demo").innerHTML="Salom"
+// })
+// document.addEventListener("mouseover",()=>{
+//     document.getElementById("demo").innerHTML="Salom"
+// })
+// document.addEventListener("mouseout",()=>{
+//     document.getElementById("demo").innerHTML="Salom"
+// })
+// document.addEventListener("mousemove",()=>{
+//     document.getElementById("demo").innerHTML=Math.random()
+// })
+
